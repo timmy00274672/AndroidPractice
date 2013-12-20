6571d1d6bb3a8d2334d80190495d58e31a985981
timmy00274672 <timmy00274672@gmail.com>
2013-12-20 15:34:52 +0800

    Create a example ClearableEditText

---
+++ example/ClearableEditText.java
@@ -0,0 +1,21 @@
+public class ClearableEditText extends LinearLayout {
+	EditText editText;
+	Button clearButton;
+	
+	public ClearableEditText(Context context) {
+		super(context);
+		
+		// Inflate the view from the layout resource.
+		String infService = Context.LAYOUT_INFLATER_SERVICE;
+		LayoutInflater li;
+		li = (LayoutInflater)getContext().getSystemService(infService);
+		li.inflate(R.layout.clearable_edit_text, this, true);
+		
+		// Get references to the child controls.
+		editText = (EditText)findViewById(R.id.editText);
+		clearButton = (Button)findViewById(R.id.clearButton);
+		
+		// Hook up the functionality
+		hookupButton();
+	}
+}
